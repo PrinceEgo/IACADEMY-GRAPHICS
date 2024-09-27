b04de48fc9bc1999b98797f54ef1d309202b1a29
PrinceEgo <barino.jm@gmail.com>
2024-09-27 21:22:06 +0800

    Project Reinitialization

---
+++ GRAPHICS/GRAPHICS/main.cpp
@@ -0,0 +1,14 @@
+
+void BasicSineAndCosine() {
+	float angle = glfwGetTime(); // Gets time-based angle
+	float x = sin(angle);        // Sine for horizontal movement
+	float y = cos(angle);        // Cosine for vertical movement
+
+}
+
+int main() 
+{
+
+
+	return 0;
+}
